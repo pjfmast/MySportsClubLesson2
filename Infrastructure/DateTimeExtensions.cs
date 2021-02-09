8e64a229b5a5356d4c9d072e11e0f69ec5557fda
Paul de Mast <pjf.demast@avans.nl>
2021-02-09 16:14:08 +0100

    Correct NextDayAt

--- Infrastructure/DateTimeExtensions.cs
+++ Infrastructure/DateTimeExtensions.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace MySportsClub.Infrastructure {
     public static class DateTimeExtensions {
         public static DateTime NextDayAt(this DateTime dt, DayOfWeek dayOfWeek, int hour, int minutes) {
-            int diff = (7 - (Math.Abs(dayOfWeek - dt.DayOfWeek))) % 7;
+            int diff = (dayOfWeek >= dt.DayOfWeek) ? (dayOfWeek - dt.DayOfWeek) : (7 + (dayOfWeek - dt.DayOfWeek));
             DateTime day = dt.AddDays(diff).Date;
             DateTime nextDayAt = day.AddHours(hour).AddMinutes(minutes);
             return nextDayAt;
